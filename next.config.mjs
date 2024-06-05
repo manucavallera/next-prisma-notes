b14c5cb1925620b3fa0d47c1ea6e271c6f811012
manucavallera <120131770+manucavallera@users.noreply.github.com>
2024-06-05 11:58:47 -0300

    prestarrt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //...
+  scripts: {
+    build: "prisma generate && next build",
+  },
+};
 
 export default nextConfig;
